81fa75c6ec31ac7571f2dab9770c4e63a4758e47
Android Studio <devandroid@ort>
2022-08-09 08:40:10 -0300

    Contador de caracteres functional

--- App.js
+++ App.js
@@ -6,22 +6,16 @@ export default function App() {
 
   const [mensaje, setMensaje] = useState('');
   const [mostrar, setMostrar] = useState('');
-  const [counter, setCounter] = useState(0);
 
 
-  handleMensaje = (text) => {
+  const handleMensaje = (text) => {
     setMensaje(text)
   }
-
-  counterCaracteres = () =>{
-    setCounter(counter+1);
-  }
-
   return (
     <View style={styles.container}>
       <View style={styles.lilContainer}>
-        <TextInput placeholder='Ingrese texto' onChangeText={{handleMensaje, counterCaracteres}} style={{ alignSelf: 'center'}}></TextInput>
-        <Text style={{ alignSelf: 'right', marginTop:5}}>{counter}</Text>
+        <TextInput placeholder='Ingrese texto' onChangeText={(text) => handleMensaje(text)} style={{ alignSelf: 'center'}}></TextInput>
+        <Text style={{ alignSelf: 'center', marginTop:5}}>Caracteres:{mensaje.length}</Text>
         <Button title='PRESIONAME' style={styles.btn} onPress={() => setMostrar(mensaje)}></Button>
         <View style={{ borderBottomColor: 'black', borderBottomWidth: 0.5, marginTop: 10, }} />
         <Text style={{ alignSelf: 'center', marginTop:10}}>{mostrar}</Text>
